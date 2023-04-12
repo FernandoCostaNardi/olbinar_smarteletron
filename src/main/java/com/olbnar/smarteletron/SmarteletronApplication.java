ec47709c63df157ee1042cfe1aac5b33ef5bcf2a
fernando.nardi <fernando.nardi@senior.com.br>
2023-04-11 21:28:52 -0300

    first commit

---
+++ src/main/java/com/olbnar/smarteletron/SmarteletronApplication.java
@@ -0,0 +1,13 @@
+package com.olbnar.smarteletron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarteletronApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmarteletronApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/olbnar/smarteletron/SmarteletronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olbnar.smarteletron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarteletronApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
